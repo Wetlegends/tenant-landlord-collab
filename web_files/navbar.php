d68b0e466e1c3fca1f8ec02e14ba0e2f8c523964
Aiden Smutek <145587227+Wetlegends@users.noreply.github.com>
2024-04-07 13:02:56 +0100

    Add files via upload

---
+++ web_files/navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar">
+    <div class="container">
+        <a href="#" class="logo">Logo</a>
+        <ul class="nav-menu">
+            <li><a href="#" class="nav-link">Home</a></li>
+            <li><a href="#" class="nav-link">Payments</a></li>
+            <li><a href="#" class="nav-link">Maintenance</a></li>
+            <li><a href="#" class="nav-link">Bookings</a></li>
+            <li><a href="#" class="nav-link">Help</a></li>
+            <li><a href="#" class="nav-link">Contact Landlord</a></li>
+            <li><a href="#" class="nav-link">Login</a></li>
+            
+        </ul>
+    </div>
+</nav>
+
+
+
+</body>
+</html>
